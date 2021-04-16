60b6ed7228cd2e41f2b932fca643de3c0ea80d5f
nehadeshpande89 <nehadeshpande1995@gmail.com>
2021-04-16 18:26:40 +0100

    first

--- mainAdmin/inc/connect.inc.php
+++ mainAdmin/inc/connect.inc.php
@@ -1,4 +1,4 @@
 <?php 
-mysqli_connect("database-2.csmnzp3t9xk5.us-east-1.rds.amazonaws.com","admin","12345678") or die("Couldn't connet to SQL server");
+$con=mysqli_connect("database-2.csmnzp3t9xk5.us-east-1.rds.amazonaws.com","admin","12345678") or die("Couldn't connet to SQL server");
 mysqli_select_db($con,"daowat_db") or die("Couldn't select DB");
  ?>
