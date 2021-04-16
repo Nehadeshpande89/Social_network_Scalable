cf941193a78879fd37340c45a893a992c699e824
nehadeshpande89 <nehadeshpande1995@gmail.com>
2021-04-16 01:21:02 +0100

    first

--- inc/connect.inc.php
+++ inc/connect.inc.php
@@ -1,5 +1,5 @@
 <?php 
-$con=mysqli_connect("database-1.csmnzp3t9xk5.us-east-1.rds.amazonaws.com","root","") or die("Couldn't connet to SQL server");
+$con=mysqli_connect("database-2.csmnzp3t9xk5.us-east-1.rds.amazonaws.com","admin","12345678") or die("Couldn't connet to SQL server");
 mysqli_select_db($con,"daowat_db") or die("Couldn't select DB");
 
 //time formate
